d644e27e794877609243013324118fddf478119d
Fui03 <120361352+Fui03@users.noreply.github.com>
2024-05-21 13:40:11 +0800

    Minor changes due to cahnging of firebase to JS SDK

--- screens/Home.tsx
+++ screens/Home.tsx
@@ -1,10 +1,29 @@
 import React from 'react';
-import { SafeAreaView, Text, StyleSheet } from 'react-native';
+import { SafeAreaView, Text, StyleSheet , Button} from 'react-native';
+import { getAuth, signOut } from "firebase/auth";
+import type { NativeStackNavigationProp } from "@react-navigation/native-stack";
+import { useNavigation} from "@react-navigation/native";
+
+
+
 
 const Home = () => {
+
+  const HandleSignOut = () => {
+    const auth = getAuth();
+    signOut(auth).then(() => {
+        navigation.replace('Login');
+    }).catch((error) => {
+
+    });
+  }
+  const navigation = useNavigation<NativeStackNavigationProp<any>>();
   return (
     <SafeAreaView style={styles.container}>
+
+      <Button title = "Sign Out" onPress={HandleSignOut}/>
       <Text>Home</Text>
+      
     </SafeAreaView>
   );
 };
